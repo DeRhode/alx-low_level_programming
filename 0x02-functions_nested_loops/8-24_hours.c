7441b89d8316e780ba8cc9c8915eb520e8e5419b
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-06-21 10:55:28 +0000

    task 7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * jack_bauer: Entry point
- * Functions that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - Print every minute of the day of jack
  *
  * Return: Always 0 (Success)
  */
