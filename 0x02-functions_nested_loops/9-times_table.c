cb2268c84539ae9dcd1af06760a524725574d32d
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-06-21 11:37:17 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 int result = i * j;
-if (result < 10)
+/*if (result < 10)
 {
 _putchar(' ');
 _putchar('0' + result);
@@ -23,7 +23,7 @@ else
 {
 _putchar('0' + (result / 10));
 _putchar('0' + (result % 10));
-}
+} */
 if (j < 9)
 {
 _putchar(',');
