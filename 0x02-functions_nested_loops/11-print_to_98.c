cbb56db0bae9e74d6ea70d76dcdf32bb05c760f0
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-06-21 16:54:44 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,14 +21,40 @@ void print_to_98(int n)
 int i;
 for (i = n; i <= 98; i++)
 {
+
+
+if (i >= 0 && i < 10 )
+{
+_putchar('0' + i);
+}
+
+
+if (i >= 10 && i <= 98)
+{
 _putchar('0' + (i / 10));
 _putchar('0' + (i % 10));
+}
+
+if (i <= -10)
+{
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));
+}
+
+if (i > -10 && i < 0)
+{
+_putchar('0' + i);
+}
+
+
 if (i != 98)
 {
 _putchar(',');
-}
 _putchar(' ');
 }
+
+}
+
 for (i = n; i > 98; i--)
 {
 _putchar('0' + (i / 10));
@@ -36,7 +62,8 @@ _putchar('0' + (i % 10));
 if (i != 98)
 {
 _putchar(',');
-}
 _putchar(' ');
+}
+
 }
 }
