848b6b47ac83f253d4d483b4e6694c6a20d3b760
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-06-21 20:51:12 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,7 +20,7 @@ void print_to_98(int n)
 {
 if (n >= 98)
 {
-while(n >= 98)
+while (n >= 98)
 {
 printf("%d", n--);
 if (n != 99)
@@ -32,10 +32,10 @@ printf(" ");
 }
 else
 {
-while(n <= 98)
+while (n <= 98)
 {
 printf("%d", n++);
-if(n <= 98)
+if (n <= 97)
 {
 printf(",");
 printf(" ");
