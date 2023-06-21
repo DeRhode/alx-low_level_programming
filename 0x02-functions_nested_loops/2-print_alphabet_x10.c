cfce155662b4ca9b76b9721d1a7ee500e88c51cc
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-06-21 07:50:09 +0000

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,21 @@
-int print_alphabet(char c)
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_X10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet_x10(void)
 {
-char letter = c;
-for (int a = 0; a < 10; a++)
+int a;
+char letter;
+for (a = 0; a < 10; a++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
 }
+_putchar('\n');
+}
+return (0);
 }
