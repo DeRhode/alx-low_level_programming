229e1ae5a6f5caa3ca93df112d17aa1a1ae1184b
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-07-02 07:57:42 +0000

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,6 +10,7 @@
 void print_rev(char *s)
 {
 int length = 0;
+int i;
 if (s == NULL)
 {
 return;
@@ -18,7 +19,7 @@ while (s[length] != '\0')
 {
 length++;
 }
-for (int i = length - 1; i >= 0; i--)
+for (i = length - 1; i >= 0; i--)
 {
 putchar(s[i]);
 }
