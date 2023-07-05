1c3599fab377673a8458bc092bc4ea7114c01174
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-07-05 11:42:46 +0000

    Starting with RECURSION

---
+++ 0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - Write a function that prints
+ * a string, followed by a new line.
+ * @s: String to be entered into the function
+ *
+ * Return: Always 0;
+ */
+void _puts_recursion(char *s)
+{
+
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point for execution of the functions
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+return (0);
+}
