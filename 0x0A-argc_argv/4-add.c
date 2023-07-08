a9c2366dc90a34871bae4011d534e6c7026ee61f
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-07-08 17:39:13 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
  * main - Entry point
  * @argc: first
@@ -8,6 +10,28 @@
  */
 int main(int argc, char *argv[])
 {
-
+int sum = 0;
+int num;
+int i;
+int j;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i =1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+num = atoi(argv[i]);
+sum += num;
+}
+printf("%d\n", sum);
 return (0);
 }
