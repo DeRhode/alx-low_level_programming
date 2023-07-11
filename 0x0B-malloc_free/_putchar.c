c2a3d5b1a12b54e0123209c3841649d93d1f6a3b
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-07-11 01:09:25 +0000

    malloc

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c
+ * @c: The printed character
+ *
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
