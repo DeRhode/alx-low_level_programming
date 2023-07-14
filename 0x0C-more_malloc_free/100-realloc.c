1c00d42f6922dc273076751035e96604d45b7013
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-07-14 09:05:08 +0000

    tasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,18 +12,21 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *n_ptr;
-if (ptr == NULL)
-return malloc(new_size);
-if (new_size == 0)
-free(ptr);
-return (NULL);
-if (new_size == old_size)
-return ptr;
-n_ptr = malloc(new_size);
-if (n_ptr == NULL)
-return (NULL);
-memcpy(n_ptr, ptr, (old_size < new_size) ? old_size : new_size);
-free(ptr);
-return (n_ptr);
+	void *n_ptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	n_ptr = malloc(new_size);
+	if (n_ptr == NULL)
+		return (NULL);
+	memcpy(n_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+	free(ptr);
+	return (n_ptr);
 }
