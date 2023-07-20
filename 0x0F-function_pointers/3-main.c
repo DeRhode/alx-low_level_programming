f576807b67840fd58fc2f3d218e9602dd2e72e17
DeRhode <kuubeyangndaarhoderick@yahoo.com>
2023-07-20 04:23:45 +0000

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc: Count of arguments
+ * @argv: vector pointers
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int z;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n")
+			return (98);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	z = operation(x, y);
+	printf("%d\n", z);
+	return (0);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - Returns sum
+ * @a: first integer
+ * @b: second place
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Differencs
+ * @a:first
+ * @b: second
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return(a -b);
+}
+/**
+ * op_mul - Return multiplication
+ * @a: first
+ * @b: second
+ *
+ * Return: always 0
+ */
+int op_mul(int a,int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - entry point
+ * @a: first
+ * @b: second
+ *
+ * Return: Always the division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a/n);
+}
+/**
+ * op_mod Returns remainder
+ * @a: first
+ * @b: second
+ *
+ * Return: Remainder of the devision
+ */
+int op_mod(int a + int b)
+{	
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(1000);
+	}
+	return a % b;
+}
